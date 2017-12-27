8c7b348635560372211da804604d5b057929ea4d
Etki <etki@etki.me>
2017-12-27 21:38:01 +0300

    Added missing serializer import

--- src/Entity/Schema/MappingType.php
+++ src/Entity/Schema/MappingType.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AmaTeam\ElasticSearch\Schema\Entity\Schema;
 
 use AmaTeam\ElasticSearch\Schema\API\Entity\Schema\MappingTypeInterface;
+use JMS\Serializer\Annotation as Serializer;
 
 class MappingType implements MappingTypeInterface
 {
